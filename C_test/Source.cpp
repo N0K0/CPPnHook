e936c4e309c2a4817edba72bc052af94dc559867
Nikolas <nikolasp@ifi.uio.no>
2016-12-06 19:32:24 +0100

    Opening, Reading, Writing memory works now. Next up: Thread Hijacking and pointer-chains :)

--- C_test/Source.cpp
+++ C_test/Source.cpp
@@ -12,8 +12,8 @@ DWORD GetProcessID(char * exe_name){
 
 	if (Process32First(snapshot, &entry) == TRUE){
 		while (Process32Next(snapshot, &entry) == TRUE){
-			printf("ID: %d Name: %s\n", entry.th32ProcessID, entry.szExeFile);
 			if (strncmp(entry.szExeFile, exe_name, _MAX_PATH) == 0){
+				printf("ID: %d Name: %s\n", entry.th32ProcessID, entry.szExeFile);
 				CloseHandle(snapshot);
 				return entry.th32ProcessID;
 			}
@@ -21,7 +21,6 @@ DWORD GetProcessID(char * exe_name){
 	}
 	CloseHandle(snapshot);
 	return NULL;
-	return;
 }
 
 
@@ -29,7 +28,7 @@ DWORD GetThreads(DWORD process_ID, std::vector<DWORD> vec){
 
 	THREADENTRY32 entry;
 	entry.dwSize = sizeof(THREADENTRY32);
-	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, process_ID);
+	HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
 
 	if (Thread32First(snapshot, &entry) == TRUE) {
 		while (Thread32Next(snapshot, &entry) == TRUE) {
@@ -43,16 +42,54 @@ DWORD GetThreads(DWORD process_ID, std::vector<DWORD> vec){
 	return NULL;
 }
 
+template<typename T>
+T readMemory(HANDLE proc, LPVOID adr) {
+	T val;
+	if (ReadProcessMemory(proc, adr, &val, sizeof(T), NULL) == 0) {
+		error();
+	}
+	return val;
+}
+template<typename T>
+void writeMemory(HANDLE proc, LPVOID adr, T val) {
+	WriteProcessMemory(proc, adr, &val, sizeof(T), NULL);
+}
+
+void error() {
+	char err[256];
+	memset(err, 0, 256);
+	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), err, 255, NULL);
+}
+
 int main(void) {
-	DWORD wnd_handle = GetProcessID("procexp64.exe");
-	if (wnd_handle == NULL) {
+	DWORD proc_handle = GetProcessID("notepad.exe");
+	if (proc_handle == NULL) {
 		perror("Find Window");
 		return -1;
 	}
 
 	std::vector<DWORD> vec;
+	GetThreads(proc_handle,vec);
+	
+	HANDLE open_proc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, proc_handle);
+	if (open_proc == NULL) error();
 
-	GetThreads(wnd_handle,vec);
+	printf("Open Proc: %d\n", open_proc);
+		
+
+	_int64 base = 0x6B2137C859;
+	for (__int64 i = base; i < (base + 1000); i = i + 1 ) {
+		auto pre = readMemory<BYTE>(open_proc, (LPVOID)i);
+		writeMemory<BYTE>(open_proc, (LPVOID)i, 0x90);
+		auto post = readMemory<BYTE>(open_proc, (LPVOID)i);
+
+		printf("(%x): %x\t(%x)\n", i, pre, post);
+	}
+
+
+	Sleep(2000);
+
 
 	return 0;
 }
